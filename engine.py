aff2bd63e3ec885b39f32d5b24f713431294218f
SavvyHex <paisaketh@gmail.com>
2023-01-08 19:31:43 +0530

    modified:   engine.py

--- engine.py
+++ engine.py
@@ -10,5 +10,5 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
-
         self.whiteToMove = True
+        self.moveLog = list()
