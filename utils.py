5eb4227642c093098e5ef4ca07e06f6e052a6e00
SavvyHex <paisaketh@gmail.com>
2023-01-09 16:42:15 +0530

    added return type

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-def load_images():
+def load_images() -> dict:
     imgs = dict()
     for piece in ["bp", "bR", "bN", "bB", "bQ", "bK", "wQ", "wK", "wB", "wN", "wR", "wp"]:
         imgs[piece] = pg.image.load(f"assets/images/{piece}.png")
